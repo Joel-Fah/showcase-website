ab7fbfc2a5a8c147604b569a9242762cfc7dbeee
Dejon Fah Joël Xavier <joelfah2003@gmail.com>
2021-12-04 04:44:45 +0100

    Creation of navbar and footer

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('services/', services, name='services'),
+    path('realisation/', realisation, name='realisation'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,5 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context = {}
+    context = {
+        'nbar': 'home',
+    }
     return render(request, 'home.html', context)
+
+def services(request):
+    context = {
+        'nbar': 'services',
+    }
+    return render(request, 'services.html', context)
+
+def realisation(request):
+    context = {
+        'nbar': 'realisation',
+    }
+    return render(request, 'realisation.html', context)
+
+def contact(request):
+    context = {
+        'nbar': 'contacts',
+    }
+    return render(request, 'contacts.html', context)
+
+def about(request):
+    context = {
+        'nbar': 'about',
+    }
+    return render(request, 'about.html', context)
